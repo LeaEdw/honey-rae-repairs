87e60fee7402588a5dde5eda464750342d2d53f1
Lea Edwards <leaxedw@gmail.com>
2025-11-08 12:18:08 -0600

    added the claim and close functionality to the application

--- src/components/tickets/Ticket.jsx
+++ src/components/tickets/Ticket.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { getAllEmployees } from "../../services/employeeService";
+import { assignTicket, updateTicket } from "../../services/ticketService";
 
-export const Ticket = ({ ticket, currentUser }) => {
+export const Ticket = ({ ticket, currentUser, getAndSetTickets }) => {
   const [employees, setEmployees] = useState([]);
   const [assignedEmployee, setAssignedEmployee] = useState({});
 
@@ -14,18 +15,44 @@ export const Ticket = ({ ticket, currentUser }) => {
         // Set the state with the awaited data...
         setEmployees(employeesArray);
       } catch (error) {
-        console.error("Failed to fetch tickets...", error)
+        console.error("Failed to fetch tickets...", error);
       }
-    }
-    fetchTickets()
+    };
+    fetchTickets();
   }, []);
 
   useEffect(() => {
     const foundEmployee = employees.find(
       (employee) => employee.id === ticket.employeeTickets[0]?.employeeId
-    )
-    setAssignedEmployee(foundEmployee)
+    );
+    setAssignedEmployee(foundEmployee);
   }, [employees, ticket]);
+
+  const handleClaim = () => {
+    const currentEmployee = employees.find(
+      (employee) => employee.userId === currentUser.id
+    );
+    const newEmployeeTicket = {
+      employeeId: currentEmployee.id,
+      serviceTicketId: ticket.id,
+    };
+    assignTicket(newEmployeeTicket).then(() => {
+      getAndSetTickets();
+    });
+  };
+
+  const handleClose = () => {
+    const closedTicket = {
+      id: ticket.id,
+      userId: ticket.userId,
+      description: ticket.description,
+      emergency: ticket.emergency,
+      dateCompleted: new Date(),
+    }
+    updateTicket(closedTicket).then(() => {
+      getAndSetTickets()
+    })
+  }
 
   return (
     <section className="ticket" key={ticket.id}>
@@ -35,9 +62,11 @@ export const Ticket = ({ ticket, currentUser }) => {
 
       <footer>
         <div>
-            <div className="ticket-info">Assignee</div>
-                <div>{assignedEmployee ? assignedEmployee.user?.fullName: "None"}</div>
-            
+          <div className="ticket-info">Assignee</div>
+          <div>
+            {assignedEmployee ? assignedEmployee.user?.fullName : "None"}
+          </div>
+
           <div className="ticket-info">Emergency</div>
 
           <div>{ticket.emergency ? "yes" : "no"}</div>
@@ -45,8 +74,21 @@ export const Ticket = ({ ticket, currentUser }) => {
         <div className="btn-container">
           {/* If the logged in user is an employee and there's no employee ticket associated with the service tickets,
           then a button to claim the ticket should display */}
+          {currentUser.isStaff && !assignedEmployee ? (
+            <button className="btn btn-secondary" onClick={handleClaim}>
+              Claim
+            </button>
+          ) : (
+            ""
+          )}
           {/* If the logged in user is the assigned employee for the ticket and there is no dateCompleted, then a button
           to close the ticket should display */}
+          {assignedEmployee?.userId === currentUser.id &&
+          !ticket.dateCompleted ? (
+            <button className="btn btn-warning" onClick={handleClose}>Close</button>
+          ) : (
+            ""
+          )}
         </div>
       </footer>
     </section>
