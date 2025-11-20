878c81624cabeafebbb41015bcfd3f660aa6e45a
Lea Edwards <leaxedw@gmail.com>
2025-11-19 23:48:22 -0600

    added show open tickets, show all tickets, and delete button functionality to customer views

--- src/components/tickets/TicketFilterBar.jsx
+++ src/components/tickets/TicketFilterBar.jsx
@@ -1,26 +1,43 @@
-export const FilterBar = ({ setShowEmergency, setSearchTerm }) => {
+export const FilterBar = ({ setShowEmergency, setSearchTerm, currentUser, setShowOpenOnly }) => {
   return (
     <div className="filter-bar">
-      <div className="btn-container">
-        <button
-          className="filter-btn btn-warning"
-          onClick={() => setShowEmergency(true)}
-        >
-          Emergency
-        </button>
-        <button
-          className="filter-btn btn-info"
-          onClick={() => setShowEmergency(false)} // This resets the filter
-        >
-          Show All
-        </button>
-        <input
-          onChange={(event) => setSearchTerm(event.target.value)}
-          type="text"
-          placeholder="Search Tickets ..."
-          className="ticket-search"
-        ></input>
-      </div>
+      {currentUser.isStaff ? (
+        <>
+          <div className="btn-container">
+            <button
+              className="filter-btn btn-warning"
+              onClick={() => setShowEmergency(true)}
+            >
+              Emergency
+            </button>
+            <button
+              className="filter-btn btn-info"
+              onClick={() => setShowEmergency(false)} // This resets the filter
+            >
+              Show All
+            </button>
+            <input
+              onChange={(event) => setSearchTerm(event.target.value)}
+              type="text"
+              placeholder="Search Tickets ..."
+              className="ticket-search"
+            ></input>
+          </div>
+        </>
+      ) : (
+        <>
+          <button 
+            className="filter-btn btn-primary">
+              Create Ticket
+            </button>
+          <button 
+            className="filter-btn btn-info"
+            onClick={() =>{setShowOpenOnly(true)}}>Open Tickets</button>
+          <button   
+            className="filter-btn btn-secondary"
+            onClick={() => {setShowOpenOnly(false)}}>All My Tickets</button>
+        </>
+      )}
     </div>
   );
 };
--- src/services/ticketService.jsx
+++ src/services/ticketService.jsx
@@ -1,24 +1,40 @@
 export const getAllTickets = async () => {
-    const response = await fetch(`http://localhost:8088/serviceTickets?_embed=employeeTickets`)
-    return await response.json()
-}
+  const response = await fetch(
+    `http://localhost:8088/serviceTickets?_embed=employeeTickets`
+  );
+  return await response.json();
+};
 
 export const assignTicket = async (employeeTicket) => {
-    return fetch("http://localhost:8088/employeeTickets", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(employeeTicket),
-    })
-}
+  return fetch("http://localhost:8088/employeeTickets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(employeeTicket),
+  });
+};
 
 export const updateTicket = async (ticket) => {
-    return fetch(`http://localhost:8088/serviceTickets/${ticket.id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(ticket),
-    })
-}
+  return fetch(`http://localhost:8088/serviceTickets/${ticket.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(ticket),
+  });
+};
+
+export const deleteTicket = async (ticketId) => {
+  const response = await fetch(
+    `http://localhost:8088/serviceTickets/${ticketId}`,
+    {
+      method: "DELETE",
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to delete post. Status: ${response.status}`);
+  }
+
+  return response;
+};
