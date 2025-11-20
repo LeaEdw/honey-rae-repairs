4da122a6ec4910cf97c4d63d98b2c61ec6750404
Lea Edwards <leaxedw@gmail.com>
2025-11-19 20:08:34 -0600

    finished employee edit

---
+++ src/views/EmployeeViews.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+import { NavBar } from "../components/navigation/NavBar";
+import { Welcome } from "../components/welcome/welcome";
+import { TicketList } from "../components/tickets/TicketList";
+import { EmployeesList } from "../components/employees/EmployeesList";
+import { CustomerList } from "../components/customers/CustomersList";
+import { CustomerDetails } from "../components/customers/CustomerDetails";
+import { EmployeeForm } from "../components/forms/EmployeeForms";
+
+export const EmployeeViews = ( {currentUser} ) => {
+    return (
+
+        <Routes>
+            <Route
+                path="/"
+                element={
+                    <>
+                        <NavBar />
+                        <Outlet />
+                    </>
+                }
+            >
+                <Route index element={<Welcome />} />
+                <Route path="tickets" element={<TicketList currentUser={currentUser} />} />
+                <Route path="employees">
+                    <Route index element={<EmployeesList />} />
+                    <Route path=":employeeId" element={<EmployeeDetails />} />
+                </Route>
+                <Route path="customers">
+                    <Route index element={<CustomerList />} />
+                    <Route path=":customerId" element={<CustomerDetails />} />
+                </Route>
+                <Route path="profile" element={<EmployeeForm currentUser={currentUser} />} />
+            </Route>
+        </Routes>
+
+    );
+    
+}
